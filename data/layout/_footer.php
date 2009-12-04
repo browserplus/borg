8552436794df9cb65c64b4f0306c955a0feb6a6b
Steve Spencer <steve@bigfrog.net>
2009-12-04 11:19:03 -0800

    add analytics tracking code

--- data/layout/_footer.php
+++ data/layout/_footer.php
@@ -1,5 +1,23 @@
         <!-- div id="ft">
         </div -->
     </div>
-    </body>
+<?php if (get_cfg_var("bp_env") != "local"): ?>
+
+<script type="text/javascript">
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-11920567-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script');
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.setAttribute('async', 'true');
+    document.documentElement.firstChild.appendChild(ga);
+})();
+
+</script>
+
+<?php endif ?>
+</body>
 </html>
