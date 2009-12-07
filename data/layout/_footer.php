b1808cdf7b539a17c58d563281053e2d25c4b9c6
Steve Spencer <steve@bigfrog.net>
2009-12-07 10:35:18 -0800

    Add Create Commons license

--- data/layout/_footer.php
+++ data/layout/_footer.php
@@ -1,5 +1,9 @@
         <div id="ft">
-
+            <span xmlns:dc="http://purl.org/dc/elements/1.1/" href="http://purl.org/dc/dcmitype/InteractiveResource" property="dc:title" rel="dc:type">BrowserPlus</span> by 
+            <a xmlns:cc="http://creativecommons.org/ns#" href="http://browserplus.org" property="cc:attributionName" rel="cc:attributionURL">Yahoo!</a> is licensed under a 
+            <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/">Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License</a>.
+            <br /><br />
+            <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/us/88x31.png" /></a>
         </div>
     </div>
 <?php if (get_cfg_var("bp_env") != "local"): ?>
