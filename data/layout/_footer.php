2ce0a1aa4bf1761b9358b2503385b125e372a639
Steve Spencer <steve@bigfrog.net>
2009-12-08 17:05:59 -0800

    git is not working with me on this footer thing

--- data/layout/_footer.php
+++ data/layout/_footer.php
@@ -1,6 +1,10 @@
         <div id="ft">
 
+            This site is licensed under a
+            <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/">Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License</a>.
+            <br /><br />
             <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/us/88x31.png" /></a>
+
 
         </div>
     </div>
