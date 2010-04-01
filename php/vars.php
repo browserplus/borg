61142c1ed1529295c2fe7063a5da2965a53e12d6
Steve Spencer <steve@bigfrog.net>
2010-04-01 12:28:54 -0700

    latest uploader is now 1.1.1

--- php/vars.php
+++ php/vars.php
@@ -2,5 +2,5 @@
 define('BROWSERPLUS_JS_VERSION', '2.6.0');
 define('RUBY_SERVICE_VERSION',   '4.2.6');
 define('INSTALL_WIDGET_VERSION', '1.0.11');
-define('UPLOAD_WIDGET_VERSION',  '1.1.0');
+define('UPLOAD_WIDGET_VERSION',  '1.1.1');
 define('NOTIFY_WIDGET_VERSION',  '1.0.2');
