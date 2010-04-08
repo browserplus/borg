96abb8c58bb8b05f84dbc458d570e23cc74c62f2
Lloyd Hilaiel <lloyd@hilaiel.com>
2010-04-08 10:35:57 -0600

    up the post limit to 256kb and gzip before storing on disk

--- www/diagnose/save.php
+++ www/diagnose/save.php
@@ -1,12 +1,13 @@
 <?php
 
-define("MAX", 64*1024);
+define("MAX", 256*1024);
 
 $dir = "/tmp/diagnose";
 $logs = (isset($_POST["logs"]) ? $_POST["logs"] : null);
 
 if ($logs && strlen($logs) <= MAX) {
 	if ((is_dir($dir) && is_writable($dir)) || mkdir($dir, 0777, true)) {
+        $logs = gzencode($logs);
 		$filename = tempnam($dir, "d1_" . date("Ymd_H_i_s") . "_");
 		file_put_contents($filename, $logs);
 		echo "OK";
