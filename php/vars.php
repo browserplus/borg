aa728434a53be3f78722de29e0dbfd16f897b8cf
Gordon Durand <gordon_durand@yahoo.com>
2010-08-09 18:10:13 -0700

    Bump bp.js to 2.9.9

--- php/vars.php
+++ php/vars.php
@@ -1,5 +1,5 @@
 <?php
-define('BROWSERPLUS_JS_VERSION', '2.9.8');
+define('BROWSERPLUS_JS_VERSION', '2.9.9');
 define('RUBY_SERVICE_VERSION',   '4.2.6');
 define('INSTALL_WIDGET_VERSION', '1.0.11');
 define('UPLOAD_WIDGET_VERSION',  '1.1.2');
