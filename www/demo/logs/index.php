3ad67bb93d6385ff932fc3d2c81afe55959a93d4
Steve Spencer <steve@bigfrog.net>
2010-11-09 17:11:05 -0800

    working version

--- www/demo/logs/index.php
+++ www/demo/logs/index.php
@@ -6,13 +6,17 @@ require("../../../php/vars.php");
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 	<title>Log File Viewer</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/combo?3.2.0/build/tabview/assets/skins/sam/tabview.css&3.2.0/build/widget/assets/skins/sam/widget.css&3.2.0/build/cssfonts/fonts-min.css">
+	<style type="text/css" media="screen">
+	body{margin:10px;}
+	h1 { margin-top:0em;font: bold 16pt	 'Courier New', Courier, monospace !important;}
+	pre {font-size:93%;}
+	.yui3-tabview-panel {overflow:scroll;height:600px;}
+	</style>
 </head>
-<body class="yui3-skin-sam  yui-skin-sam"> 
-<h1>View BrowserPlus Log Files</h1>
+<body class="yui3-skin-sam	yui-skin-sam"> 
+<h1>BrowserPlus Log Files</h1>
 
-<p>View tabs</p>
 <div id="demo"></div>
 
 	<script type="text/javascript" src="http://yui.yahooapis.com/3.2.0/build/yui/yui-min.js"></script> 
@@ -20,44 +24,53 @@ require("../../../php/vars.php");
 
 <script type="text/javascript"> 
 YUI().use("yui", "tabview", function(Y) {
-	/*
-    var tabview = new Y.TabView({
-        children: [{
-            label: 'foo',
-            content: '<p>foo content</p>'
-        }, {
-            label: 'bar',
-            content: '<p>bar content</p>'
-        }, {
-            label: 'baz',
-            content: '<p>baz content</p>'
-        }]
-    });
- 
-    tabview.render('#demo');
-    tabview.selectChild(2);
-	*/
-	
 
+	var tabview = new Y.TabView({
+		srcNode: '#demo'
+	});
+	
 	function error(s, err) {
-		console.log("Error in " + s + ": " + err.error + (err.verboseError ? " - " + err.verboseError : ""));
+		alert("Error in " + s + ": " + err.error + (err.verboseError ? " - " + err.verboseError : ""));
 	}
 
 	function getLogFiles() {
-		console.log("getLogFiles");
 		BrowserPlus.LogAccess.get({}, function(logs){
-			var i, len, children = [], item, tabview;
+			var i, len, item;
 			if (logs.success) {
+				// each item is a file handle
 				for (i = 0, len = logs.value.length; i < len; i++) {
-					item = logs[i];
-					console.log("item:");
-					console.log(item);
-					children.push({label:item.name, content: "test content: " + i});
-				}
-				tabview = new Y.TabView({children:children});
-			    tabview.render('#demo');
+					item = logs.value[i];
+
+					// cap max read amt to 768KB
+					var offset = 0;
+					var readAmt = 768 * 1024;
+
+					if (item.size > readAmt) offset = item.size - readAmt;
+
+					BrowserPlus.FileAccess.read({file: item, offset: offset,size: readAmt },
+						(function() {
+							var file = item;
+							var start = offset;
+							return function(r) {
+								var str, i;
+								if (r.success) {
+									str = r.value;
+									// inform user when log file is chopped
+									if (start != 0) {
+										str = "... Log file too large, showing most recent " + (readAmt/1024) + "KB ...\n" + 
+											str.substr(str.indexOf("\n"));
+									}
+
+									tabview.add({label:file.name, content: "<pre>"+str+"</pre>"}, tabview.size());
+
+									// need to render tabview when we get data back for first tab
+									if (tabview.size() == 1) tabview.render();
+								}
+							};
+						})());
+				} // for 
 			} else {
-				error("LOGACCESS", logs)
+				error("LogAccess", logs);
 			}
 		});
 	}
@@ -65,15 +78,13 @@ YUI().use("yui", "tabview", function(Y) {
 
 	BrowserPlus.init(function(init) {
 		if (init.success) {
-			console.log("init");
 			// lots of different services for all the various upload options
 			BrowserPlus.require({
 				services: [
-					{service: 'FileAccess',   version: "2", minversion: "2.0.1" },
-					{service: 'LogAccess',   version: "1", minversion: "1.0.0"}
+					{service: 'FileAccess',	version: "2", minversion: "2.0.1" },
+					{service: 'LogAccess',	version: "1", minversion: "1.0.0"}
 				]},
 				function(require) {
-					console.log("require");
 					if (require.success) {
 						getLogFiles();
 					} else {
